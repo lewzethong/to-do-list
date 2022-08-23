1367910a59c9de1dadd3a264659cfee84fde348c
Lew Ze Thong <lewzethong@gmail.com>
2022-08-24 01:18:41 +0800

    get all task function done

--- application.js
+++ application.js
@@ -1,14 +1,20 @@
-$.ajax({
-  type:'GET',
-  url:'https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=1',
-  dataType: 'json',
-  success: function(response,textStatus){
-    response.tasks.forEach(function(task) {
-      console.log(task.content)
-    })
-  },
-  error: function(request, textStatus,errorMessage){
-    console.log(errorMessage)
-  }
-
+$(document).ready(function () {
+  updateAllTask();
 })
+
+var updateAllTask = function () {
+    $.ajax({
+    type:'GET',
+    url:'https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=1',
+    dataType: 'json',
+    success: function(response,textStatus){
+      response.tasks.forEach(function(task) {
+        $('#todo-list').append('<p>' + task.content + '</p>')
+      })
+    },
+    error: function(request, textStatus,errorMessage){
+      console.log(errorMessage)
+    }
+  })
+}
+  
