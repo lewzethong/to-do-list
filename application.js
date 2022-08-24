8c052953b639cfd149e51084ae89c4dee9245d86
Lew Ze Thong <lewzethong@gmail.com>
2022-08-24 15:19:03 +0800

    add task function done

--- application.js
+++ application.js
@@ -1,5 +1,10 @@
 $(document).ready(function () {
   updateAllTask();
+
+  $('#create-task').on('submit', function (e) {
+    e.preventDefault();
+    addTask();
+  })
 })
 
 var updateAllTask = function () {
@@ -11,6 +16,28 @@ var updateAllTask = function () {
       response.tasks.forEach(function(task) {
         $('#todo-list').append('<p>' + task.content + '</p>')
       })
+    },
+    error: function(request, textStatus,errorMessage){
+      console.log(errorMessage)
+    }
+  })
+}
+
+var addTask = function () {
+  $.ajax({
+    type:'POST',
+    url:'https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=1',
+    contentType: 'application/json',
+    dataType: 'json',
+    data: JSON.stringify({
+      task: {
+        content: $('#new-task').val()
+      }
+    }),
+    success: function(response,textStatus){
+      $('#todo-list').empty();
+      $('#new-task').val('');
+      updateAllTask();
     },
     error: function(request, textStatus,errorMessage){
       console.log(errorMessage)
